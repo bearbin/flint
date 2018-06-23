546b7cc3224f130ffa0174df377e8b3a8dcebcc7
Franklin Schrans <f.schrans@me.com>
2018-06-23 13:53:49 +0100

    Use Truffle

---
+++ examples/flightmanager/truffle.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const Web3 = require("web3");
+const web3 = new Web3();
+const WalletProvider = require("truffle-wallet-provider");
+const Wallet = require('ethereumjs-wallet');
+const config = require("./config.js");
+
+var privateKey = new Buffer(config.airlinePrivateKey, "hex")
+var wallet = Wallet.fromPrivateKey(privateKey);
+
+var ropstenProvider = new WalletProvider(wallet, "https://ropsten.infura.io/");
+
+module.exports = {
+  networks: {
+    ropsten: {
+      provider: ropstenProvider,
+      gas: 4600000,
+      gasPrice: web3.utils.toWei("25", "gwei"),
+      network_id: "3",
+    }
+  }
+};
