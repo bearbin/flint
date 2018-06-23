e53fc0161554365c2c46a8a2132c2a4505ca1a10
Franklin Schrans <f.schrans@me.com>
2018-06-23 17:32:19 +0100

    Add web demo

--- examples/flightmanager/migrations/1_initial_migration.js
+++ examples/flightmanager/migrations/1_initial_migration.js
@@ -2,7 +2,7 @@ var FlightManager = artifacts.require("./FlightManager.sol")
 const config = require('../config.js')
 
 module.exports = function(deployer, network, accounts) {
-  const flightID = "IC217"
+  const flightID = web3.toHex("IC217")
   const admin = config.airlinePublicKey
   const ticketPrice = 2000000000000000000
   const numTickets = 240
